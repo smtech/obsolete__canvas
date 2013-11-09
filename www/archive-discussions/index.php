e4ed6a3c4643a9e3eac399ce7fe34cc51eb08d82
Seth Battis <SethBattis@stmarksschool.org>
2013-11-09 10:10:50 -0500

    Restructure dev-archive-discussions
    Restructuring this Archive Discussions to reflect the new dev-support branch.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- www/archive-discussions/index.php
+++ www/archive-discussions/index.php
@@ -1,22 +1,21 @@
 <?php
 
+require_once(__DIR__ . '/../config.inc.php');
+
 /* what Canvas API user are we going to connect as? */
-require_once('../.ignore.read-only-authentication.inc.php');
+require_once(APP_PATH . '/.ignore.read-only-authentication.inc.php');
 
 /* configurable options */
-require_once('config.inc.php');
+require_once(__DIR__ . '/config.inc.php');
 
 /* handles HTML page generation */
-require_once('../page-generator.inc.php');
+require_once(APP_PATH . '/include/page-generator.inc.php');
 
 /* handles the core of the Canvas API interactions */
-require_once('../canvas-api.inc.php');
+require_once(APP_PATH . '/include/canvas-api.inc.php');
 
 /* handles working directory functions */
-require_once('../working-directory.inc.php');
-
-/* we do directly work with Pest on some AWS API calls */
-require_once('../Pest.php');
+require_once(APP_PATH . '/include/working-directory.inc.php');
 
 // TODO: implement an import as well as an export!
 
